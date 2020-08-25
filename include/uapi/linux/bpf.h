ad6f77ad4ee2a64aef4a0bcd51ba801c87970129
Jiri Olsa <jolsa@kernel.org>
2020-08-25 21:21:20 +0200

    BACKPORT: bpf: Add d_path helper
    Adding d_path helper function that returns full path for
    given 'struct path' object, which needs to be the kernel
    BTF 'path' object. The path is returned in buffer provided
    'buf' of size 'sz' and is zero terminated.
    
      bpf_d_path(&file->f_path, buf, size);
    
    The helper calls directly d_path function, so there's only
    limited set of function it can be called from. Adding just
    very modest set for the start.
    
    Updating also bpf.h tools uapi header and adding 'path' to
    bpf_helpers_doc.py script.
    
    Change-Id: If390ec6189a537b730b9ae595d374cbfa83f6f5b
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Acked-by: KP Singh <kpsingh@google.com>
    Link: https://lore.kernel.org/bpf/20200825192124.710397-11-jolsa@kernel.org
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -2772,6 +2772,18 @@ union bpf_attr {
  *		Dynamically cast a *sk* pointer to a *udp6_sock* pointer.
  *	Return
  *		*sk* if casting is valid, or NULL otherwise.
+ *
+ * long bpf_d_path(struct path *path, char *buf, u32 sz)
+ *	Description
+ *		Return full path for given 'struct path' object, which
+ *		needs to be the kernel BTF 'path' object. The path is
+ *		returned in the provided buffer 'buf' of size 'sz' and
+ *		is zero terminated.
+ *
+ *	Return
+ *		On success, the strictly positive length of the string,
+ *		including the trailing NUL character. On error, a negative
+ *		value.
  */
 #define __BPF_FUNC_MAPPER(FN)		\
 	FN(unspec),			\
@@ -2914,7 +2926,15 @@ union bpf_attr {
 	FN(skc_to_tcp_sock),		\
 	FN(skc_to_tcp_timewait_sock),	\
 	FN(skc_to_tcp_request_sock),	\
-	FN(skc_to_udp6_sock),
+	FN(skc_to_udp6_sock),		\
+	FN(get_task_stack),		\
+	FN(load_hdr_opt),		\
+	FN(store_hdr_opt),		\
+	FN(reserve_hdr_opt),		\
+	FN(inode_storage_get),		\
+	FN(inode_storage_delete),	\
+	FN(d_path),			\
+	/* */
 
 /* integer value in 'imm' field of BPF_CALL instruction selects which helper
  * function eBPF program intends to call
