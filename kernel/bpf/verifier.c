77cb005397873c6f3d57f7d10e3a943dd1f7271c
Lorenz Bauer <lmb@cloudflare.com>
2020-09-21 13:12:23 +0100

    UPSTREAM: bpf: Make context access check generic
    Always check context access if the register we're operating on is
    PTR_TO_CTX, rather than relying on ARG_PTR_TO_CTX. This allows
    simplifying the arg_type checking section of the function.
    
    Change-Id: Ic9ab0542797d42272d61a3e0e71aead2adc89a4e
    Signed-off-by: Lorenz Bauer <lmb@cloudflare.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20200921121227.255763-8-lmb@cloudflare.com
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -3549,9 +3549,6 @@ static int check_func_arg(struct bpf_verifier_env *env, u32 arg,
 		expected_type = PTR_TO_CTX;
 		if (type != expected_type)
 			goto err_type;
-		err = check_ctx_reg(env, reg, regno);
-		if (err < 0)
-			return err;
 	} else if (arg_type == ARG_PTR_TO_SOCK_COMMON) {
 		expected_type = PTR_TO_SOCK_COMMON;
 		/* Any sk pointer can be ARG_PTR_TO_SOCK_COMMON */
@@ -3628,6 +3625,10 @@ static int check_func_arg(struct bpf_verifier_env *env, u32 arg,
 				regno);
 			return -EACCES;
 		}
+	} else if (type == PTR_TO_CTX) {
+		err = check_ctx_reg(env, reg, regno);
+		if (err < 0)
+			return err;
 	}
 
 	if (reg->ref_obj_id) {
