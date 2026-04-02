a4a43aebd203ec02adba5accf997bcf990c047b8
Sorayukii <sorayukii69@gmail.com>
2026-04-02 18:02:08 +0700

    tcp: Force the system to prioritize low latency
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -84,7 +84,7 @@
 #include <crypto/hash.h>
 #include <linux/scatterlist.h>
 
-int sysctl_tcp_low_latency __read_mostly;
+int sysctl_tcp_low_latency __read_mostly = 1;
 
 #ifdef CONFIG_TCP_MD5SIG
 static int tcp_v4_md5_hash_hdr(char *md5_hash, const struct tcp_md5sig_key *key,
